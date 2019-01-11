039ab4ce370d2ae79e5e8898d24f2a1d1bcb44d7
0x00-pl <0x00.pl@gmail.com>
2019-01-11 05:45:35 +0000

    modified:   generator.py

--- generator.py
+++ generator.py
@@ -5,7 +5,7 @@ import numpy as np
 import pack_npy
 
 
-class Generator:
+class FullData:
     def __init__(self, filename, info_filename):
         self.up = pack_npy.Unpacker(filename, np.load(info_filename))
 
@@ -23,6 +23,28 @@ class Generator:
                     inputs.append(i_arr)
                     outputs.append(o_arr)
                 yield np.array(inputs), np.array(outputs)
+
+    def __len__(self):
+        return len(self.up.info)
+
+
+class InputPartData:
+    def __init__(self, filename, info_filename):
+        self.up = pack_npy.Unpacker(filename, np.load(info_filename))
+
+    def __call__(self, num_items_each_i_part, i_part_shape, batch_size=32, *args, **kwargs):
+        while True:
+            inputs = []
+            outputs = []
+            info_list = random.choices(self.up.info, k=batch_size)
+            for info in info_list:
+                istart, ilen, idtype, ishape, ostart, olen, odtype, oshape = info
+                isize = np.dtype(idtype).itemsize
+                part_start = random.randrange(0, int(ilen/isize) - num_items_each_i_part)
+                i_arr, o_arr = self.up.get_data_part(info, part_start, i_part_shape, isize)
+                inputs.append(i_arr)
+                outputs.append(o_arr)
+            yield np.array(inputs), np.array(outputs)
 
     def __len__(self):
         return len(self.up.info)
